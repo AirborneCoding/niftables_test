2e729b13d2c8151684059a29c06bf721f487cf43
AirborneCoding <airborneCoding@gmail.com>
2024-05-05 10:05:55 +0100

    edit

--- src/components/home/HeaderSection.tsx
+++ src/components/home/HeaderSection.tsx
@@ -5,6 +5,7 @@ import { Heading } from "../layouts/Heading";
 import { Text } from "../layouts/Text";
 import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "@/context";
+import { Img } from "../layouts/Img";
 
 
 const HeaderSection = () => {
@@ -25,10 +26,19 @@ const HeaderSection = () => {
 
             <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent via-blue-950 to-black-900 opacity-90"></div>
 
-            <div className="flex flex-col items-end justify-center gap-[481px] pb-[102px] pt-[30px] md:gap-[360px] md:pb-5 sm:gap-60 sm:py-5 relative">
-                <button onClick={openSidebar} className="text-blue-A200 hidden md:inline mr-16">
-                    <FaBars size={30} />
-                </button>
+            <div className="flex flex-col justify-center gap-[481px] pb-[102px] pt-[30px] md:gap-[360px] md:pb-5 sm:gap-60 sm:py-5 relative">
+                <div className="md:justify-between hidden md:flex mr-16 mx-5">
+                    <Img
+                        src="img_header_logo.png"
+                        width={140}
+                        height={39}
+                        alt="header logo"
+                        className="h-[39px] w-[140px] object-contain"
+                    />
+                    <button onClick={openSidebar} className="text-blue-A200 ">
+                        <FaBars size={30} />
+                    </button>
+                </div>
                 <div className="p-5"></div>
 
                 <div className="body-container flex flex-col items-start bg-white rounded-lg shadow-lg">
